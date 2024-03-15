1e437e27a60fba5954c07a9965c6bf9ac2d942e3
João Lucas Felix de Macena <131909907+Joao-lucas-felix@users.noreply.github.com>
2024-03-15 20:53:49 -0300

    Ex1018

---
+++ src/Beginner/Ex1018.java
@@ -0,0 +1,18 @@
+package Beginner;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Ex1018 {
+    public static void main(String[] args) {
+        int i = new Scanner(System.in).nextInt();
+        System.out.println(i);
+        List<Integer> list  = new ArrayList<>(List.of(100, 50, 20, 10, 5, 2, 1));
+
+        for(int j =0; j < list.size(); j++){
+            System.out.printf("%d nota(s) de R$ %d,00\n", i/list.get(j),list.get(j));
+            i = i % list.get(j);
+        }
+    }
+}
